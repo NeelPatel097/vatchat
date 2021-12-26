727a5eadee8518ea91a64b6cc8c7d99cc9c80c69
Neel Patel <np7.patel.np@gmail.com>
2021-12-26 21:38:29 +0530

    User Registration and validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+const { UserInputError } = require('apollo-server')
 const { User } = require('../models')
 module.exports = {
     Query: {
@@ -10,4 +12,50 @@ module.exports = {
         }
       },
     },
+    Mutation: {
+      register: async (_, args) => {
+        let { username, email, password, confirmPassword } = args
+        let errors = {}
+
+        try {
+          // Validate input data
+          if(email.trim() === '') errors.email = 'Email must not be empty'
+          if(username.trim() === '') errors.username = 'username must not be empty'
+          if(password.trim() === '') errors.password = 'password must not be empty'
+          if(confirmPassword.trim() === '') errors.confirmPassword = 'repeat password must not be empty'
+
+          if(password !== confirmPassword) errors.confirmPassword = 'passwords must match'
+
+          //  Check if username / email exists
+          // const userByUsername = await User.findOne({ where: { username}})
+          // const userByEmail = await User.findOne({ where: { email }})
+
+          // if(userByUsername) errors.username = 'Username is taken'
+          // if(userByEmail) errors.email = 'Email is taken'
+
+          if(Object.keys(errors).length > 0){
+            throw errors
+          }
+
+          // Hash password
+          password = await bcrypt.hash(password, 6)
+
+          // Create User
+          const user = await User.create({
+            username, email, password,
+          })
+
+          // TODO: Return user
+          return user
+        } catch(err) {
+            console.log(err)
+            if(err.name === 'SequelizeUniqueConstraintError'){
+              err.errors.forEach((e) => (errors[e.path] = `${e.path} is already taken`))
+            } else if(err.name === 'SequelizeValidationError'){
+              err.errors.forEach(e => errors[e.path] = e.message)
+            }
+            throw new UserInputError ('Bad input', { errors })
+        }
+      }
+    }
   };
--- models/user.js
+++ models/user.js
@@ -15,15 +15,26 @@ module.exports = (sequelize, DataTypes) => {
   };
   User.init({
     username: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(20),
       allowNull: false,
-      unique: true
+      unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'must be a valid email address',
+        }
+      }
     },
     email: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(100),
       allowNull: false,
       unique: true
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imageUrl: DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'User',
