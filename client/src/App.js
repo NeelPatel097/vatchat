1acab84027db9b5b6dde21baa8d44da97747abc1
Neel Patel <np7.patel.np@gmail.com>
2022-01-02 23:48:37 +0530

    App Setup 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,56 @@
 import './App.scss';
-import { Card, Container, Row, Col, Form, Button } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 
 function App() {
+
+  const [variables, setVariables] = useState({
+    email: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const submitRegisterForm = e => {
+    e.preventDefault()
+  }
+
   return (
-    <Container>
-      <Row className="bg-white py-5">
+    <Container className="pt-5">
+      <Row className="bg-white py-5 justify-content-center">
         <Col sm={8} md={6} lg={4}>
           <h1 className='text-center'>Register</h1>
-          <Form>
+          <Form onSubmit={submitRegisterForm}>
             <Form.Group>
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" />
+              <Form.Control type="email" 
+                value={variables.email} 
+                onChange={(e) => 
+                setVariables({...variables, email: e.target.value})}/>
             </Form.Group>
             <Form.Group>
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control type="text" 
+                value={variables.username} 
+                onChange={(e) => 
+                setVariables({...variables, username: e.target.value})}
+              />
             </Form.Group>
             <Form.Group>
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" />
+              <Form.Control type="password" 
+                 value={variables.password} 
+                 onChange={(e) => 
+                 setVariables({...variables, password: e.target.value})}
+              />
             </Form.Group>
             <Form.Group>
               <Form.Label>Confirm password</Form.Label>
-              <Form.Control type="password" />
+              <Form.Control type="password" 
+                 value={variables.confirmPassword} 
+                 onChange={(e) => 
+                 setVariables({...variables, confirmPassword: e.target.value})}
+              />
             </Form.Group>
             <br/>
             <div className='text-center'>
